fbe64e8504ad8f1213453bb1977bd455254e1ddd
sean swe <seanswe@gmail.com>
2021-02-16 23:05:54 -0800

    updated dockerflow.py

--- dockerflow.py
+++ dockerflow.py
@@ -1,9 +1,6 @@
-import prefect
 from prefect import task, Flow
 from prefect.run_configs import DockerRun
 from prefect.storage import Docker
-
-storage = Docker(python_dependencies=["numpy"])
 
 
 @task(log_stdout=True)
@@ -13,9 +10,11 @@ def use_numpy():
     print(np.arange(10))
 
 
-with Flow("dockerflow") as flow:
+with Flow(
+    "use_numpy",
+    storage=Docker(python_dependencies=["numpy"]),
+    run_config=DockerRun(),
+) as flow:
     use_numpy()
-
-flow.run_config = DockerRun()
 
 flow.register(project_name="dockerflow")
