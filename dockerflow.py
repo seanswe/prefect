539eb43c8247b2c5dfdc7586f0f753c638e25dd6
sean swe <seanswe@gmail.com>
2021-03-03 19:10:36 -0800

    added secrets management

--- dockerflow.py
+++ dockerflow.py
@@ -10,9 +10,14 @@ def use_numpy():
     print(np.arange(10))
 
 
+awsid = Secret("AWS_ACCESS_KEY_ID").get()
+awskey = Secret("AWS_SECRET_ACCESS_KEY").get()
+
 with Flow("use_numpy") as flow:
     flow.storage = Docker(python_dependencies=["numpy"])
-    flow.run_config = DockerRun()
+    flow.run_config = DockerRun(
+        env={"AWS_ACCESS_KEY_ID": awsid, "AWS_SECRET_ACCESS_KEY": awskey}
+    )
     use_numpy()
 
 flow.register(project_name="dockerflow")
