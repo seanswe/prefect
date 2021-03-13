48e3e41859d08aef23af48c05240604eabda5932
sean swe <seanswe@gmail.com>
2021-03-12 20:02:36 -0800

    fixed location of secrets

--- dockerflow.py
+++ dockerflow.py
@@ -4,8 +4,8 @@ from prefect.storage import Docker
 from prefect.tasks.secrets import PrefectSecret
 
 
-@task(log_stdout=True)
-def use_numpy():
+@task(log_stdout=True
+def use_numpy(access_key, secret_key):
     import numpy as np
 
     print(np.arange(10))
@@ -13,12 +13,11 @@ def use_numpy():
 
 with Flow("use_numpy") as flow:
     flow.storage = Docker(python_dependencies=["numpy"])
-    flow.run_config = DockerRun(
-        env={
-            "AWS_ACCESS_KEY_ID": PrefectSecret("AWS_ACCESS_KEY_ID").get(),
-            "AWS_SECRET_ACCESS_KEY": PrefectSecret("AWS_SECRET_ACCESS_KEY").get(),
-        }
-    )
-    use_numpy()
+    flow.run_config = DockerRun()
+
+    access_key = PrefectSecret("AWS_ACCESS_KEY_ID")
+    secret_key = PrefectSecret("AWS_SECRET_ACCESS_KEY"),
+
+    use_numpy(access_key, secret_key)
 
 flow.register(project_name="dockerflow")
