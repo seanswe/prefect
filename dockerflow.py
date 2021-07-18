903875f4f8ef2256918e86c7f7e6b5d5c76789e4
sean swe <seanswe@gmail.com>
2021-07-17 20:27:40 -0700

    removed aws env vars, simplified flow

--- dockerflow.py
+++ dockerflow.py
@@ -1,23 +1,18 @@
 from prefect import task, Flow
 from prefect.run_configs import DockerRun
 from prefect.storage import Docker
-from prefect.tasks.secrets import PrefectSecret
+import numpy as np
 
 
-@task(log_stdout=True
-def use_numpy(access_key, secret_key):
-    import numpy as np
+@task(log_stdout=True)
+def use_numpy():
 
-    print(np.arange(10))
+    return np.arange(10)
 
 
 with Flow("use_numpy") as flow:
     flow.storage = Docker(python_dependencies=["numpy"])
     flow.run_config = DockerRun()
+    nptask = use_numpy()
 
-    access_key = PrefectSecret("AWS_ACCESS_KEY_ID")
-    secret_key = PrefectSecret("AWS_SECRET_ACCESS_KEY"),
-
-    use_numpy(access_key, secret_key)
-
-flow.register(project_name="dockerflow")
+# flow.register(project_name="dockerflow")
